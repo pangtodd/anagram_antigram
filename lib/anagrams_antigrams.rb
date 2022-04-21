32e7a4269de3671e98888bc62ba812dea1f7e2a6
Todd Pangilinan <pang.todd@gmail.com>
2022-04-21 14:39:55 -0700

    word_check passes test to identify if a word has a vowel

--- lib/anagrams_antigrams.rb
+++ lib/anagrams_antigrams.rb
@@ -8,4 +8,20 @@ class Anagrams
       "These words are not anagrams."
     end
   end
+
+  def word_check(word1)
+    vowels = ["a", "i", "e", "o", "u", "y"]
+    score = 0
+    word_array= word1.downcase.split("")
+    word_array.each do |letter|
+      if vowels.include?(letter)
+        score = score +1
+      end
+    end
+    if score < 1
+      "check again, your word doesn't have any vowels."
+    else
+      "ok, passes the base requirement."
+    end
+  end
 end

3fc8eddb2e4cec9b46c0c38a36eab7b2c673efd9
Todd Pangilinan <pang.todd@gmail.com>
2022-04-21 13:18:40 -0700

    anagram_check method passes test 2, can idenifty anagram regardless of capitliaztion.

--- lib/anagrams_antigrams.rb
+++ lib/anagrams_antigrams.rb
@@ -2,7 +2,7 @@ require('pry')
 
 class Anagrams
   def anagram_check(word1, word2)
-    if word1.split("").sort.join == word2.split("").sort.join
+    if word1.upcase.split("").sort.join == word2.upcase.split("").sort.join
       "These words are anagrams!"
     else
       "These words are not anagrams."

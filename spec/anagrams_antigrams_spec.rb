69945661d5054021c676357cf43ce5b2f54c8670
Todd Pangilinan <pang.todd@gmail.com>
2022-04-21 13:16:59 -0700

    test 2 for anagram_check, checks for anagrams regardless of capitaliztion.

--- spec/anagrams_antigrams_spec.rb
+++ spec/anagrams_antigrams_spec.rb
@@ -4,8 +4,12 @@ require('anagrams_antigrams')
 describe(Anagrams)do
   describe('#anagram_check') do
     it("takes two arguments and sees if the words are anagrams")do
-    test = Anagrams.new
-    expect(test.anagram_check("bob","obo")).to eq("this is an anagram")
+      test = Anagrams.new
+      expect(test.anagram_check("apt","pat")).to eq("These words are anagrams!")
+    end
+    it("takes two arguments and sees if the words are anagrams, regardless of capitalization")do  
+    test = Anagrams.new  
+    expect(test.anagram_check("Apt","Pat")).to eq("These words are anagrams!")
     end
   end
 end

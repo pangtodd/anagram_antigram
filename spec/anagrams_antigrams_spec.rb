eca07bb489a479df83f787650c89867589e563d7
Todd Pangilinan <pang.todd@gmail.com>
2022-04-21 14:12:06 -0700

    first test for word_check method, checks to make sure a word has a vowel or y.

--- spec/anagrams_antigrams_spec.rb
+++ spec/anagrams_antigrams_spec.rb
@@ -8,8 +8,14 @@ describe(Anagrams)do
       expect(test.anagram_check("apt","pat")).to eq("These words are anagrams!")
     end
     it("takes two arguments and sees if the words are anagrams, regardless of capitalization")do  
-    test = Anagrams.new  
-    expect(test.anagram_check("Apt","Pat")).to eq("These words are anagrams!")
+      test = Anagrams.new  
+      expect(test.anagram_check("Apt","Pat")).to eq("These words are anagrams!")
+    end
+  end
+  describe("#word_check")
+    it("takes two arguments and makes sure the words have letters")do
+      test = Anagrams.new
+      expect(test.word_check("bbbbbbrr")).to eq("check again, your word doesn't have any vowels.")
     end
   end
 end

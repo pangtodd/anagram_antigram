f69b35a9220366eaf60a033fdd5fdab47fa98ab9
Todd Pangilinan <pang.todd@gmail.com>
2022-04-21 12:39:07 -0700

    establish initial script file.

---
+++ anagrams_antigrams_script.rb
@@ -0,0 +1,9 @@
+#!/usr/bin/env ruby
+require ('./lib/anagrams_antigrams.rb')
+
+puts "Give me two words, and I'll tell you some things about them!"
+puts "first word?"
+word1 = gets.chomp
+puts "second word?"
+word2 = gets.chomp
+puts "first word is #{word1} and second word is #{word2}, right?"

0a107d383f07309718a0627c2e86bd843c2f9ee2
Todd Pangilinan <pang.todd@gmail.com>
2022-04-21 20:03:41 -0700

    refactored method structure, created new tests for compare_score and antigram_check methods.

--- spec/anagrams_antigrams_spec.rb
+++ spec/anagrams_antigrams_spec.rb
@@ -12,10 +12,22 @@ describe(Anagrams)do
       expect(test.anagram_check("Apt","Pat")).to eq("These words are anagrams!")
     end
   end
-  describe("#word_check")
-    it("takes two arguments and makes sure the words have letters")do
+  describe ("#compare_score")do
+    it("takes two arguements and returns a score of how many letters are the same")do
       test = Anagrams.new
-      expect(test.word_check("bbbbbbrr")).to eq("check again, your word doesn't have any vowels.")
+      expect(test.compare_score("bob", "bob")).to eq(3)
     end
   end
+  describe("#word_check")do
+    it("takes two arguments and makes sure the words have letters")do
+      test = Anagrams.new
+      expect(test.word_check("apt")).to eq("ok, passes the base requirement.")
+    end
+  end
+  describe("#antigram_check")do
+    it("takes two arguments and checks to see if they are antigrams/have any letters in common")do
+      test = Anagrams.new
+      expect(test.antigram_check("box", "cat")).to eq('box and cat are antigrams.')
+    end
+  end 
 end

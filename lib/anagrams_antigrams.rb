38ffbac60f82c8b84bc88910587bb456f80eac3b
Todd Pangilinan <pang.todd@gmail.com>
2022-04-21 20:19:51 -0700

    anagram_check and compare_score methods both pass.

--- lib/anagrams_antigrams.rb
+++ lib/anagrams_antigrams.rb
@@ -9,19 +9,34 @@ class Anagrams
     end
   end
 
-  def word_check(word1)
-    vowels = ["a", "i", "e", "o", "u", "y"]
+  def compare_score (word1, word2)
     score = 0
-    word_array= word1.downcase.split("")
-    word_array.each do |letter|
-      if vowels.include?(letter)
+    word1_array = word1.downcase.split("")
+    word2_array = word2.downcase.split("")
+    word1_array.each do |letter|
+      if word2_array.include?(letter)
         score = score +1
       end
     end
+    score
+  end
+
+  def word_check(word1)
+    vowels = "aiedouy"
+    score = compare_score(word1, vowels)
     if score < 1
       "check again, your word doesn't have any vowels."
     else
       "ok, passes the base requirement."
+    end
+  end
+
+  def antigram_check(word1, word2)
+    score = compare_score(word1, word2)
+    if score < 1
+      "#{word1} and #{word2} are antigrams."
+    else
+      "not antigrams."
     end
   end
 end

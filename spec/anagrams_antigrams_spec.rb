949d56556af61793d49ec4a5ca8d304de8eae254
Todd Pangilinan <pang.todd@gmail.com>
2022-04-22 19:52:14 -0700

    refactored the tests after deciding to refactor some of the methods. Word_check now links to anagram_check. Anagram_ch eck links to antigram_check.

--- spec/anagrams_antigrams_spec.rb
+++ spec/anagrams_antigrams_spec.rb
@@ -4,30 +4,30 @@ require('anagrams_antigrams')
 describe(Anagrams)do
   describe('#anagram_check') do
     it("takes two arguments and sees if the words or phrases are anagrams, regardless of punctuation or spacing")do
-      test = Anagrams.new
-      expect(test.anagram_check("Fourty! Five?","OVER        FIFTY!")).to eq("These words are anagrams!")
+      test = Anagrams.new("Forty! Five?","OVER        FIFTY!")
+      expect(test.anagram_check).to eq("Forty! Five? and OVER        FIFTY! are anagrams!")
     end
     it("takes two arguments and sees if the words are anagrams, regardless of capitalization")do  
-      test = Anagrams.new  
-      expect(test.anagram_check("Apt","Pat")).to eq("These words are anagrams!")
+      test = Anagrams.new("Apt","Pat")
+      expect(test.anagram_check).to eq("Apt and Pat are anagrams!")
     end
   end
   describe ("#compare_score")do
     it("takes two arguements and returns a score of how many letters are the same")do
-      test = Anagrams.new
-      expect(test.compare_score("bob", "bob")).to eq(3)
+      test = Anagrams.new("bob", "bob")
+      expect(test.compare_score("bob","bob")).to eq(3)
     end
   end
   describe("#word_check")do
-    it("takes two arguments and makes sure the words have letters")do
-      test = Anagrams.new
-      expect(test.word_check("apt")).to eq("ok, passes the base requirement.")
+    it("takes a single argument and makes sure the string has at least one vowel")do
+      test = Anagrams.new("hzzz", "hi")
+      expect(test.word_check).to eq("check again, hzzz doesn't seem to have any vowels or a y.")
     end
   end
   describe("#antigram_check")do
     it("takes two arguments and checks to see if they are antigrams/have any letters in common")do
-      test = Anagrams.new
-      expect(test.antigram_check("box", "cat")).to eq('box and cat are antigrams.')
+      test = Anagrams.new("box o' ROX!", "FAT cat")
+      expect(test.antigram_check).to eq("box o' ROX! and FAT cat are antigrams.")
     end
   end 
 end

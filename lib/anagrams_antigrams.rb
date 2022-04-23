5aec0d4abad41a65e33f249757b2a290a9a99569
Todd Pangilinan <pang.todd@gmail.com>
2022-04-23 12:30:50 -0700

    refactored using regex, eliminated compare_score. Added functionality to antigram_check where it notes how many letters are the same and which are the same if word is not an antigram, passes test.

--- lib/anagrams_antigrams.rb
+++ lib/anagrams_antigrams.rb
@@ -1,55 +1,43 @@
-require('pry') 
+require('pry')
 
 class Anagrams
   def initialize(word1, word2)
-    @word1 = word1
-    @word2 = word2 
+    @word1 = word1.gsub(/[^a-zA-Z]+/,"").downcase
+    @word2 = word2.gsub(/[^a-zA-Z]+/,"").downcase
+    @word1_plain = word1
+    @word2_plain = word2
   end
-  
+
   #checks words have at least one vowel and starts the program- if first condition is met, it daisy-chains into anagram_check, which daisy-chains into antigram_check.
   def word_check
-    vowels = "aiedouy"
-    word1_score = compare_score(@word1, vowels)
-    word2_score = compare_score(@word2, vowels)
+    word1_score = @word1.match(/[aeiouy]/) ? 1 : 0
+    word2_score = @word2.match(/[aeiouy]/) ? 1 : 0
     if word1_score > 0 && word2_score > 0
-      "ok, #{@word1} and #{@word2} pass the basic requirement of a word having at least one vowel or y. \n#{anagram_check}"
+      "ok, #{@word1_plain} and #{@word2_plain} pass the basic requirement of a word having at least one vowel or y. \n#{anagram_check}"
     elsif word1_score < 1 && word2_score > 0
-      "check again, #{@word1} doesn't seem to have any vowels or a y."
+      "check again, #{@word1_plain} doesn't seem to have any vowels or a y."
     elsif word2_score < 1 && word1_score > 0
-      "check again, #{@word2} doesn't seem to have any vowels or a y."  
+      "check again, #{@word2_plain} doesn't seem to have any vowels or a y."  
     else
-      "check again, #{@word1} and #{@word2} doesn't seem to have any vowels or a y."
+      "check again, #{@word1_plain} and #{@word2_plain} doesn't seem to have any vowels or a y."
     end
   end
 
   #checks for anagrams, but also calls antigram_check if not an anagram.
   def anagram_check
-    if @word1.gsub(/[^a-zA-Z]+/,"").downcase.split("").sort.join == @word2.gsub(/[^a-zA-Z]+/,"").downcase.split("").sort.join
-      "#{@word1} and #{@word2} are anagrams!"
+    if @word1.split("").sort.join == @word2.split("").sort.join
+      "#{@word1_plain} and #{@word2_plain} are anagrams!"
     else
-      "#{@word1} and #{@word2} are not anagrams. \n#{antigram_check}"
+      "#{@word1_plain} and #{@word2_plain} are not anagrams. \n#{antigram_check}"
     end
   end
   
   def antigram_check
-    score = compare_score(@word1, @word2)
-    if score < 1
-      "#{@word1} and #{@word2} are antigrams."
+    score = @word1.scan(/[#{@word2}]/)
+    if score.length() < 1
+      "#{@word1_plain} and #{@word2_plain} are antigrams."
     else
-      "#{@word1} and #{@word2} are not antigrams."
+      "#{@word1_plain} and #{@word2_plain} are not antigrams or anagrams- but #{score.length()} letters match: #{score.join(",")}"
     end
-  end
-
-  #utility method used within antigram_check and word_check.
-  def compare_score (word1, word2)
-    score = 0
-    word1_array = word1.gsub(/[^a-zA-Z]+/,"").downcase.split("")
-    word2_array = word2.gsub(/[^a-zA-Z]+/,"").downcase.split("")
-    word1_array.each do |letter|
-      if word2_array.include?(letter)
-        score = score +1
-      end
-    end
-    score
   end
 end

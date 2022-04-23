217a57561ae2bd719ffe7198a5c873443cf3e0f9
Todd Pangilinan <pang.todd@gmail.com>
2022-04-22 19:55:23 -0700

    upate to script,  seems to function properly.

--- anagrams_antigrams_script.rb
+++ anagrams_antigrams_script.rb
@@ -7,3 +7,5 @@ word1 = gets.chomp
 puts "second word?"
 word2 = gets.chomp
 puts "first word is #{word1} and second word is #{word2}, right?"
+test= Anagrams.new(word1, word2)
+puts "#{test.word_check}"

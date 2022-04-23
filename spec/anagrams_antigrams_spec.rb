95e3dc5a7bf886dc9aee75e131674a183725e036
Todd Pangilinan <pang.todd@gmail.com>
2022-04-23 12:37:57 -0700

    refactored tests, added test for branching logic of anagram_check, if not anagram, should check if it is an antigram.

--- spec/anagrams_antigrams_spec.rb
+++ spec/anagrams_antigrams_spec.rb
@@ -7,24 +7,25 @@ describe(Anagrams)do
       test = Anagrams.new("Forty! Five?","OVER        FIFTY!")
       expect(test.anagram_check).to eq("Forty! Five? and OVER        FIFTY! are anagrams!")
     end
-    it("takes two arguments and sees if the words are anagrams, regardless of capitalization")do  
-      test = Anagrams.new("Apt","Pat")
+    it("takes two arguments and sees if the words are anagrams. If they are not, it checks if they are antigrams.")do  
+      test = Anagrams.new("Apt","Part")
       expect(test.anagram_check).to eq("Apt and Pat are anagrams!")
     end
   end
   describe("#word_check")do
-    it("takes a single argument and makes sure the string has at least one vowel")do
+    it("takes a single argument and makes sure the string has at least one vowel or y")do
       test = Anagrams.new("hzzz", "hi")
       expect(test.word_check).to eq("check again, hzzz doesn't seem to have any vowels or a y.")
     end
   end
   describe("#antigram_check")do
-    it("takes two arguments and checks to see if they are antigrams/have any letters in common")do
+    it("takes two arguments and checks to see if they are antigrams/have any letters in common regardless of punctionuation, capitaliztion or special characters.")do
       test = Anagrams.new("box o' ROX!", "FAT cat")
       expect(test.antigram_check).to eq("box o' ROX! and FAT cat are antigrams.")
     end
-    it("takes two arguments and checks to see if thy are antigrams- if they are not, it says how many letters are in common, and which ones.")
+    it("takes two arguments and checks to see if thy are antigrams- if they are not, it says how many letters are in common, and which ones.")do
       test = Anagrams.new("box o' ROCKS!", "FAT cats")
-      expect(test.antigram_check).to eq("box o' ROCKS! and FAT cats are not antigrams or anagrams- but 2 letters match: k,s")
+      expect(test.antigram_check).to eq("box o' ROCKS! and FAT cats are not antigrams or anagrams- but 2 letters match: c,s")
+    end
   end 
 end
